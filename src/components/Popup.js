1cc48394501436b23f9a3e859118d3b3ef47a561
Artyom Voloshin <artyom-first@yandex.ru>
2022-05-14 18:13:40 +0500

    исправлено удаление

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,16 +1,17 @@
 export default class Popup {   // класс для работы со всплывающими окнами (ВО)
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {  // открыть ВО
     this._popup.classList.add('popup_opened');
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {   // закрыть ВО
     this._popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
   _handleEscClose(evt) {   // закрыть ВО нажатием на "Escape"
